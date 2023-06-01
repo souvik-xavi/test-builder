0a423f748ae280a7c9b5a1f398a118ce5c479af1
souvik-xavi <souvikghata2@gmail.com>
2023-06-01 23:04:39 +0530

    changes:
    clear

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,15 +3,15 @@ import { runBuilderIOCommand } from '@/test'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: any
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
+  console.log("CALLED")
   const builders=await runBuilderIOCommand('npm run builder -h');
   console.log(builders)
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name : builders })
 }
