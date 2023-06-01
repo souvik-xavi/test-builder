3a2fedb891f77702d5fd55cad0e1eeb2b2edfe9b
souvik-xavi <souvikghata2@gmail.com>
2023-06-01 23:59:54 +0530

    change

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -12,7 +12,7 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   console.log("CALLED")
-  const builders=await runBuilderIOCommand('npm run-script builder');
+  const builders=await runBuilderIOCommand('npm list');
   console.log(builders)
   res.status(200).json({ name : builders ,
                           hello:'Hello'})
